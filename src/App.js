ce7e4da3fcc9415ddf49fee2def55d09183f6b3a
Inseop Jeong <art.isjeong@gmail.com>
2019-04-13 18:29:43 +0900

    배열 데이터 렌더하는 부분 추가함

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '정인섭',
+  'birthday' : '930423',
+  'gender' : '남자',
+  'job' : '모빌씨앤씨'
+},{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '황윤성',
+  'birthday' : '911222',
+  'gender' : '남자',
+  'job' : 'LS글로벌'
+},{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이준규',
+  'birthday' : '910915',
+  'gender' : '남자',
+  'job' : '???'
+}]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {  
+          customers.map(c => {
+            return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />  
+            );
+          })
+        }
       </div>
     );
   }
