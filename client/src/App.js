f677e57eb107b0d23cc066ee3b425642698bb290
Inseop Jeong <art.isjeong@gmail.com>
2019-04-19 21:19:09 +0900

    client and server 연동

--- client/src/App.js
+++ client/src/App.js
@@ -20,30 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '정인섭',
-  'birthday' : '930423',
-  'gender' : '남자',
-  'job' : '모빌씨앤씨'
-},{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '황윤성',
-  'birthday' : '911222',
-  'gender' : '남자',
-  'job' : 'LS글로벌'
-},{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '이준규',
-  'birthday' : '910915',
-  'gender' : '남자',
-  'job' : '???'
-}]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -60,7 +54,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+            { this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -7,8 +7,29 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([{
+        'id' : 1,
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : '정인섭',
+        'birthday' : '930423',
+        'gender' : '남자',
+        'job' : '모빌씨앤씨'
+      },{
+        'id' : 2,
+        'image' : 'https://placeimg.com/64/64/2',
+        'name' : '황윤성',
+        'birthday' : '911222',
+        'gender' : '남자',
+        'job' : 'LS글로벌'
+      },{
+        'id' : 3,
+        'image' : 'https://placeimg.com/64/64/3',
+        'name' : '이준규',
+        'birthday' : '910915',
+        'gender' : '남자',
+        'job' : '???'
+      }]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
