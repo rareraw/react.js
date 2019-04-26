70e7eccd5e2850606a1a606564ae512dd8b46c28
Inseop Jeong <art.isjeong@gmail.com>
2019-04-26 20:13:37 +0900

    mysql 연동

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,29 +8,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  prot: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([{
-        'id' : 1,
-        'image' : 'https://placeimg.com/64/64/1',
-        'name' : '정인섭',
-        'birthday' : '930423',
-        'gender' : '남자',
-        'job' : '모빌씨앤씨'
-      },{
-        'id' : 2,
-        'image' : 'https://placeimg.com/64/64/2',
-        'name' : '황윤성',
-        'birthday' : '911222',
-        'gender' : '남자',
-        'job' : 'LS글로벌'
-      },{
-        'id' : 3,
-        'image' : 'https://placeimg.com/64/64/3',
-        'name' : '이준규',
-        'birthday' : '910915',
-        'gender' : '남자',
-        'job' : '???'
-      }]);
+    connection.query(
+      "SELECT *FROM customer",
+      (err, rows, fields) => {
+        console.log(rows);
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
